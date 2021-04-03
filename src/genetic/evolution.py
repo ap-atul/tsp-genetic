a9558b8a7eabf3a140afeb49c73d1c290799f7b9
atul <atulpatare99@gmail.com>
2021-04-03 22:44:48 +0530

    changes in solver

--- src/genetic/evolution.py
+++ src/genetic/evolution.py
@@ -1,3 +1,4 @@
+import sys
 import random
 
 class GeneticSolver:
@@ -5,23 +6,32 @@ class GeneticSolver:
         self._fitness_func, self._crossover_func, self._mutation_func = fitness_func, crossover_func, mutation_func
         self._pop_size, self._mut_prob, self._tourn_size, self._rev = pop_size, mut_prob, tourn_size, reverse
 
-    def _tournament(self, pop):
+    def _tournament(self, pop, min_max):
         tourn = [pop[random.randint(0, len(pop) - 1)] for _ in range(self._tourn_size)]
         fitness = [self._fitness_func(gene) for gene in tourn]
-        return pop[fitness.index(max(fitness))]
+        return pop[fitness.index(min_max(fitness))]
 
     def evolve(self, pop, gen_size):
-        for _ in range(gen_size + 1):
+        min_max = max if self._rev else min
+        best_sol, best_fit = [], sys.maxsize
+
+        for g in range(gen_size + 1):
             pop = sorted(pop, key= lambda gene: self._fitness_func(gene), reverse=self._rev)
-            new_pop = pop[0: 2]
+            
+            if (x := self._fitness_func(pop[0])) < best_fit:
+                best_fit, best_sol = x, pop[0]
+
+            print(f"Generation {g} :: {pop[0]}")
+            print(f"Fitness :: {self._fitness_func(pop[0])} \n")
+
+            new_pop = pop[0: 5]
             for i in range(int(len(pop) / 2) - 1):
-                parent_one, parent_two = self._tournament(pop), self._tournament(pop)
+                parent_one, parent_two = self._tournament(pop, min_max), self._tournament(pop, min_max)
                 child_one, child_two = self._crossover_func(parent_one, parent_two)
                 child_one, child_two = self._mutation_func(child_one, self._mut_prob), self._mutation_func(child_two, self._mut_prob)
                 new_pop += [child_one, child_two]
             pop = new_pop
 
-            for p in pop:
-                print(p)
+        print(f"Best so far :: {best_sol} {best_fit}")
+        return sorted(pop, key= lambda gene: self._fitness_func(gene), reverse=self._rev)[0]
 
-        return pop
